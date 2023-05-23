ee84e083cab831f26da5148c41295b41316a8032
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-23 16:49:33 +0200

    add script, template & style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,54 @@
 <script>
+import Cards from './components/Cards.vue';
+import axios from 'axios';
+
+export default {
+    components: {
+        Cards,
+    },
+
+    data() {
+        return {
+            CardJSON: [],
+        }
+    },
+
+    computed: {
+        getCardAPI(){
+            // axios.post('https://db.ygoprodeck.com/api/v7/randomcard.php')
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+                .then(response => this.CardJSON = response.data.data);
+        },
+    }
+}
+
 </script>
 
 <template>
-</template>
+    <header>
+        <h1>YU-GI-OH API</h1>
+    </header>
+    <div id="divCard">
+        <Cards v-for="card in CardJSON"
+        :id="card.id"
+        :name="card.name"
+        :type="card.type"
+        :img="card.card_images[0].image_url"
+        />
+    </div>
+    
+</template>    
+    
 
-<style scoped>
+<style lang="scss">
+    body {
+        margin: 0;
+    }
+    #divCard {
+        padding: 2em;
+        display: flex;
+        flex-wrap: wrap;
+        background-color: orange;
+        gap: .3em;
+    }
 </style>
