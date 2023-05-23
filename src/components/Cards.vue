deabf125fd460b704f06a584daf6d897f364edaf
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-23 16:49:58 +0200

    add script, template & style

---
+++ src/components/Cards.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    props: {
+        id: Number,
+        name: String,
+        type: String,
+        img: String,
+    }
+}
+</script>
+
+<template>
+    <div>
+        <img :src="img" :alt="id">
+        <span>{{ name }}</span>
+        <span>{{ type }}</span>    
+    </div>
+    
+</template>
+
+<style lang="scss" scoped>
+    div {
+        display: flex;
+        flex-direction: column;
+        width: calc(100% / 5 - .3em);
+        align-items: center;
+
+        img {
+            width: 100%;
+        }
+        span {
+            max-width: max-content;
+        }
+    }
+</style>
